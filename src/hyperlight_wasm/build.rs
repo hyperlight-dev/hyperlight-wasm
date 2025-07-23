ac9092b57209a1f0fabd1267c0f346a3e86485a5
Doru Blânzeanu <dblnz@pm.me>
2025-07-24 00:29:32 +0300

    Enable native debugging for wasm modules/components
    Signed-off-by: Doru Blânzeanu <dblnz@pm.me>

--- src/hyperlight_wasm/build.rs
+++ src/hyperlight_wasm/build.rs
@@ -125,7 +125,7 @@ fn build_wasm_runtime() -> PathBuf {
     env_vars.retain(|(key, _)| !key.starts_with("CARGO_"));
 
     let mut cargo_cmd = std::process::Command::new(&cargo_bin);
-    let cmd = cargo_cmd
+    let mut cmd = cargo_cmd
         .arg("build")
         .arg("--profile")
         .arg(cargo_profile)
@@ -137,6 +137,12 @@ fn build_wasm_runtime() -> PathBuf {
         .envs(env_vars)
         .env("PATH", path_with(&toolchain_dir))
         .env("HYPERLIGHT_GUEST_TOOLCHAIN_ROOT", &toolchain_dir);
+
+    // Add --features gdb if the gdb feature is enabled for this build script
+    if std::env::var("CARGO_FEATURE_GDB").is_ok() {
+        cmd = cmd.arg("--features").arg("gdb");
+    }
+
     let status = cmd
         .status()
         .unwrap_or_else(|e| panic!("could not run cargo build wasm_runtime: {}", e));
@@ -238,6 +244,10 @@ fn main() -> Result<()> {
     writeln!(file, "{}", hash_str).unwrap();
 
     println!("cargo:rerun-if-changed=build.rs");
+
+    cfg_aliases::cfg_aliases! {
+        gdb: { all(feature = "gdb", debug_assertions) },
+    }
 
     Ok(())
 }
